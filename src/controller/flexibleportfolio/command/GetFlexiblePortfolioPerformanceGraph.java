856575ff41a296f443eb11859c53669062b7391c
Anant Moudgalya <anant9798@gmail.com>
2022-11-17 12:13:35 -0500

    - added controller command and view for performance graph

--- src/controller/flexibleportfolio/command/GetFlexiblePortfolioPerformanceGraph.java
+++ src/controller/flexibleportfolio/command/GetFlexiblePortfolioPerformanceGraph.java
@@ -1,6 +1,10 @@
 package controller.flexibleportfolio.command;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Scanner;
 
 import model.flexibleportfolio.FlexiblePortfolioList;
@@ -17,8 +21,85 @@ public class GetFlexiblePortfolioPerformanceGraph implements FlexiblePortfolioCo
     this.view = view;
   }
 
+  private boolean validPortfolioName(String[] pNamesList, String pName) {
+    return Arrays.asList(pNamesList).contains(pName);
+  }
+
+  private boolean toContinue(String flag) {
+    if ((flag.equals("N") || flag.equals("n"))) {
+      return true;
+    } else {
+      return !flag.equals("Y") && !flag.equals("y");
+    }
+  }
 
   @Override
   public void go(Scanner scan) throws IOException {
+    // get portfolio value on a date
+    String[] pNames = fpList.getPortfolioListNames();
+    if (pNames.length < 1) {
+      view.noPortfoliosMessage();
+      return;
+    }
+    view.displayListOfPortfolios(pNames);
+
+    while (true) {
+      view.portfolioNamePrompt();
+      String pName = scan.next().toLowerCase();
+      if (pName.equals("0")) {
+        break;
+      }
+
+      if (!validPortfolioName(fpList.getPortfolioListNames(), pName)) {
+        view.portfolioNameErrorMessage();
+        continue;
+      }
+
+      view.rangeFromDatePrompt();
+      String dateString1 = scan.next();
+      if (dateString1.equals("0")) {
+        break;
+      }
+
+      LocalDate date1;
+      try {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        date1 = LocalDate.parse(dateString1, formatter);
+      } catch (Exception e) {
+        view.invalidDateStringMessage(dateString1);
+        continue;
+      }
+
+      view.rangeToDatePrompt();
+      String dateString2 = scan.next();
+      if (dateString2.equals("0")) {
+        break;
+      }
+
+      LocalDate date2;
+      try {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        date2 = LocalDate.parse(dateString2, formatter);
+      } catch (Exception e) {
+        view.invalidDateStringMessage(dateString2);
+        continue;
+      }
+
+      Map<String, Float> portfolioPerformance;
+      try {
+        portfolioPerformance = fpList.getPortfolio(pName)
+                .getPortfolioPerformance(date1, date2);
+      } catch (Exception e) {
+        view.displayErrorPrompt("Flexible Portfolio Performance Graph Failed! Error: " + e);
+        continue;
+      }
+
+      view.displayPerformanceGraph(portfolioPerformance, dateString1, dateString2, pName);
+
+      view.continuePrompt();
+      if (toContinue(scan.next())) {
+        break;
+      }
+    }
   }
 }
